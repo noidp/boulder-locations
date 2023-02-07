c1d39b4e6c4067177c4afeabaf419b81033b61be
Dion <dion@superlatief.nl>
2023-02-07 12:22:55 +0100

    Added demo content.

--- src/app/locations.ts
+++ src/app/locations.ts
@@ -8,10 +8,17 @@ export interface Locations {
 
 export const locations = [
   {
-    id: 1,
-    name: 'Beest boulder Den Haag',
+    id: '4ce85ee5-c4d3-4293-a64a-a3bec7f6ace0',
+    name: 'Beest Boulders',
     city: 'Den Haag',
     score: 7,
-    createdAt: Math.floor(new Date().getTime() / 1000.0),
+    createdAt: '2023-02-05T15:37:11.072Z',
+  },
+  {
+    id: '6b46539b-13ef-44a2-9886-c71a67414a6d',
+    name: 'Kunststof',
+    city: 'Leiden',
+    score: 8,
+    createdAt: '2023-02-05T15:37:32.637Z',
   },
 ];
