b04c7496503880f8b2b6018ff82e2a0de40a8379
Dion <dion@superlatief.nl>
2023-02-07 15:26:40 +0100

    Added edit component and route

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Locations, locations } from '../locations';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent {}
+export class EditComponent implements OnInit {
+  location: Locations | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // First get the location from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const locationIdFromRoute = routeParams.get('locationId');
+
+    // Find the location that correspond with the id provided in route.
+    this.location = locations.find(
+      (location) => location.id === locationIdFromRoute
+    );
+  }
+}
