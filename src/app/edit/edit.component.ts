fad74406fc251cd65b998411844c2f0c808bc52f
Dion <dion@superlatief.nl>
2023-02-16 22:17:38 +0100

    Remove demo location list.

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -2,27 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
-import { Locations, locations } from '../locations';
-
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
-  location: Locations | undefined;
-
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     // First get the location from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const locationIdFromRoute = routeParams.get('locationId');
-
-    // Find the location that correspond with the id provided in route.
-    this.location = locations.find(
-      (location) => location.id === locationIdFromRoute
-    );
   }
 
   onSubmit(form: NgForm) {
--- src/app/location-details/location-details.component.ts
+++ src/app/location-details/location-details.component.ts
@@ -4,8 +4,6 @@ import { GET_GYM } from '../graphql/graphql.queries';
 import { ActivatedRoute } from '@angular/router';
 import { ModalService } from '../modal.service';
 
-import { Locations, locations } from '../locations';
-
 @Component({
   selector: 'app-location-details',
   templateUrl: './location-details.component.html',
@@ -15,8 +13,6 @@ export class LocationDetailsComponent implements OnInit {
   boulderingGym: any[];
   loading = true;
   error: any;
-
-  location: Locations | undefined;
 
   constructor(
     private apollo: Apollo,
@@ -28,11 +24,6 @@ export class LocationDetailsComponent implements OnInit {
     // First get the location from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const locationIdFromRoute = routeParams.get('locationId');
-
-    // Find the location that correspond with the id provided in route.
-    this.location = locations.find(
-      (location) => location.id === locationIdFromRoute
-    );
 
     // Apollo
     this.apollo
